384dd651374ab76328940eb16f0545fb366bdfe3
ussuratoncachi <redchindaniil@gmail.com>
2023-09-15 00:32:16 +0700

    Basic design start

--- front/main.js
+++ front/main.js
@@ -1,12 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: { preload: path.join(__dirname, 'preload.js') }
     })
 
-    win.loadFile('index.html')
+    win.loadFile('dist/index.html')
 }
 
 function main() {
---
+++ front/src/assets/scripts/script.js
@@ -0,0 +1,26 @@
+const filenames = [
+    "2c32c35da827fad015c4664c699edd07.webp",
+    "7692ca2a02e9f6c6e6dbfb4d5ea1b073.webp",
+    "838343ddbe7b4fe946152e3c0cd552d9.webp",
+    "9140b5aa24b8474c8870bd70017e24f3.webp",
+    "98_9j3cdC1w.webp",
+    "9KxPXaSBW9Q.webp",
+    "9eO6maQiFGo.webp",
+    "9fbdd26407902676b370d3efb789dde9.webp",
+];
+
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+  }
+
+const files_elem = document.getElementById("files");
+
+filenames.forEach(name => {
+    let html = `<div class="file image" style="background-image: url(assets/images/fileimg/${name});"></div>`;
+    console.log(html)
+    files_elem.appendChild(createElementFromHTML(html));
+});
