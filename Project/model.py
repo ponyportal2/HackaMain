ec0bb57e25d8652cc86dc8d20839f5ba687e04fc
Shark Meridith <sharkmer@in-b5.nsk.21-school.ru>
2023-09-17 13:34:04 +0700

    added telegram bot etc.

--- Project/model.py
+++ Project/model.py
@@ -12,7 +12,7 @@ from authlib.integrations.flask_client import OAuth
 from sql_main import main_sql_func
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
+from werkzeug.datastructures import FileStorage
 
 from thumb_gen import *
 from sql_funct import *
@@ -27,13 +27,14 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # pbkdf2:sha256:600000$V8fkPte4iaVXhRNW$d1e84745562ce3c038cb7330164123b9d35cc871b3aa522d817adfaa34ef4b00
 # pbkdf2:sha256:600000$V8fkPte4iaVXhRNW$d1e84745562ce3c038cb7330164123b9d35cc871b3aa522d817adfaa34ef4b00
 
-@app.route("/api/login/", methods=["POST", "GET"]) # TESTED
+
+@app.route("/api/login/", methods=["POST", "GET"])  # TESTED
 @cross_origin()
 def login():
     data = request.json
     # input: {login, password}
     data_error_check(data)
-    
+
     username = data.get('login')
     password = data.get('password')
 
@@ -43,20 +44,22 @@ def login():
     if sql_username_exists(username) == False:
         return jsonify({'status': 'no_user'})
     elif check_password_hash(sql_get_user_password_hash(username), password):
-        auth_token = generate_password_hash(f'{username}_%_{password}_%_{current_time_string}') 
+        auth_token = generate_password_hash(
+            f'{username}_%_{password}_%_{current_time_string}')
         sql_change_auth_token(username, auth_token)
         print(f'Signed in with token: {auth_token}')
         return jsonify({'status': 'ok', 'token': auth_token})
     else:
         return jsonify({'status': 'invalid'})
 
-@app.route("/api/register/", methods=["POST"]) # TESTED
+
+@app.route("/api/register/", methods=["POST"])  # TESTED
 @cross_origin()
 def register():
     # input: {login, password}
     data = request.json
     data_error_check(data)
-    
+
     username = data.get('login')
     password = data.get('password')
 
@@ -66,13 +69,14 @@ def register():
         sql_add_user(username, generate_password_hash(password))
         return jsonify({'status': 'success'})
 
-@app.route("/api/verify_token/", methods=["POST"]) # WORKS
+
+@app.route("/api/verify_token/", methods=["POST"])  # WORKS
 @cross_origin()
 def verify_token():
     # input: {token}
     data = request.json
     data_error_check(data)
-    
+
     token = data.get('token')
     print(f'Token: {token} does exists: {sql_token_exists_in_db(token)}')
     if sql_token_exists_in_db(token) == True:
@@ -80,8 +84,6 @@ def verify_token():
         return jsonify({'status': 'valid', 'user': f'{username}'})
     else:
         return jsonify({'status': 'invalid'}), 400
-
-
 
 
 def send_to_backuper(the_file, file_path):
@@ -94,13 +96,14 @@ def send_to_backuper(the_file, file_path):
     backup_server_url = "http://127.0.0.1:5002/api/upload_file_backup/"
     response = requests.post(backup_server_url, files=files)
 
-@app.route("/api/upload_file/", methods=["POST"]) # WORKS
+
+@app.route("/api/upload_file/", methods=["POST"])  # WORKS
 @cross_origin()
 def upload_file():
     # input: {token, filename} , file
     if 'file' not in request.files:
         flash('No file part')
-        return jsonify({'status': 'error'}), 400 # No file part
+        return jsonify({'status': 'error'}), 400  # No file part
 
     username = sql_token_to_user(request.form['token'])
     file_path = f'users/{username}/{request.form["filename"]}'
@@ -110,13 +113,13 @@ def upload_file():
 
     if file_path == '':
         flash('No selected file')
-        return jsonify({'status': 'error'}), 400 # No selected file
+        return jsonify({'status': 'error'}), 400  # No selected file
 
     username = sql_token_to_user(request.form['token'])
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
     if file and allowed_file(file_path):
-        print("\n"+ file_path+"\n")
+        print("\n" + file_path+"\n")
         with open(file_path, "wb") as fs:
             fs.write(file)
         sql_post_image_location(username, f'{request.form["filename"]}')
@@ -127,7 +130,8 @@ def upload_file():
 
         return jsonify({'status': 'success'}), 200
 
-@app.route("/api/mv_file/", methods=["POST"]) # WORKS
+
+@app.route("/api/mv_file/", methods=["POST"])  # WORKS
 @cross_origin()
 def mv_file():
     # input: {token, filename_from, filename_into}
@@ -141,14 +145,16 @@ def mv_file():
     os.makedirs(os.path.dirname(file_to), exist_ok=True)
     try:
         shutil.move(file_from, file_to)
-        sql_change_image_location(user_id, data.get('filename_into'), data.get('filename_into'))
+        sql_change_image_location(user_id, data.get(
+            'filename_into'), data.get('filename_into'))
         return jsonify({'status': 'success'}), 200
     except:
         if not os.listdir(os.path.dirname(file_to)):
             os.rmdir(os.path.dirname(file_to))
         return jsonify({'status': 'no_from_file'}), 200
 
-@app.route("/api/del_file/", methods=["POST"]) # WORKS
+
+@app.route("/api/del_file/", methods=["POST"])  # WORKS
 @cross_origin()
 def del_file():
     # input: {token, filename}
@@ -166,7 +172,8 @@ def del_file():
     sql_remove_image_location(user_id, f'{data.get("filename")}')
     return jsonify({'status': 'success'}), 200
 
-@app.route("/api/get_all_files/", methods=["POST"]) # WORKS
+
+@app.route("/api/get_all_files/", methods=["POST"])  # WORKS
 @cross_origin()
 def get_all_files():
     # {token, pattern} (паттерн по типу \*/\*.jpg и т.п.)
@@ -176,12 +183,14 @@ def get_all_files():
     username = sql_token_to_user(data.get('token'))
     print_table("users")
     print_table("pictures")
-    initial = sql_get_all_user_pictures_with_pattern(username, data.get('pattern'))
+    initial = sql_get_all_user_pictures_with_pattern(
+        username, data.get('pattern'))
     to_return = [item[0] for item in initial]
     print(to_return)
     return jsonify({'returned': to_return})
 
-@app.route("/api/get_all_folders/", methods=["POST"]) # WORKS
+
+@app.route("/api/get_all_folders/", methods=["POST"])  # WORKS
 @cross_origin()
 def get_all_folders():
     # {token}
@@ -191,7 +200,8 @@ def get_all_folders():
     username = sql_token_to_user(data.get('token'))
     return jsonify({'returned': list_folders_in_directory(f'users/{username}')})
 
-@app.route("/api/create_folder/", methods=["POST"]) # WORKS
+
+@app.route("/api/create_folder/", methods=["POST"])  # WORKS
 @cross_origin()
 def create_folder_req():
     # {token}
@@ -201,7 +211,8 @@ def create_folder_req():
     username = sql_token_to_user(data.get('token'))
     return jsonify({'returned': create_folder(f'users/{username}/{data.get("folder")}')})
 
-@app.route("/api/delete_folder/", methods=["POST"]) # WORKS
+
+@app.route("/api/delete_folder/", methods=["POST"])  # WORKS
 @cross_origin()
 def delete_folder_req():
     # {token}
@@ -216,7 +227,8 @@ def delete_folder_req():
     sql_remove_image_by_pattern(user_id, f'{data.get("folder")}/%')
     return jsonify({'status': 'ok'})
 
-@app.route("/api/set_avatar_pic/", methods=["POST"]) # WORKS
+
+@app.route("/api/set_avatar_pic/", methods=["POST"])  # WORKS
 @cross_origin()
 def set_avatar_pic():
     # {token, filename}
@@ -226,10 +238,11 @@ def set_avatar_pic():
     username = sql_token_to_user(data.get('token'))
     filename = data.get('filename')
 
-    sql_change_avatar(username, filename) # ????
+    sql_change_avatar(username, filename)  # ????
     return jsonify({'status': 'success'}), 200
 
-@app.route("/api/get_avatar_pic/", methods=["POST"]) # WORKS
+
+@app.route("/api/get_avatar_pic/", methods=["POST"])  # WORKS
 @cross_origin()
 def get_avatar_pic():
     # {token}
@@ -239,7 +252,7 @@ def get_avatar_pic():
     if (token and sql_token_exists_in_db(token)):
         username = sql_token_to_user(token)
         avatar = sql_get_avatar(username)
-        if (avatar): 
+        if (avatar):
             return jsonify({'exists': True, 'returned': avatar}), 200
         else:
             return jsonify({'exists': False, 'returned': 'ur dumb'}), 200
@@ -247,7 +260,7 @@ def get_avatar_pic():
         return jsonify({'status': 'failed'}), 401
 
 
-@app.route("/api/logout/", methods=["POST"]) # TESTED
+@app.route("/api/logout/", methods=["POST"])  # TESTED
 @cross_origin()
 def logout():
     # {token}
@@ -256,6 +269,7 @@ def logout():
     token = data.get('token')
     sql_delete_token(token)
     return jsonify({'status': 'success'}), 200
+
 
 @app.route("/api/images/<path:kartinka>", methods=["GET"])
 @cross_origin()
@@ -270,13 +284,14 @@ def get_image(kartinka):
         if sql_does_image_exist(f'{user}/{kartinka}'):
             image_path = os.path.join(f'users/{user}/{kartinka}')
             if not os.path.exists(image_path):
-                return jsonify({'status': 'error'}), 400 # No such file
+                return jsonify({'status': 'error'}), 400  # No such file
 
             return send_file(image_path, mimetype='image/jpeg')
-        else: 
-            return jsonify({'status': 'invalid_token'}), 400 # No file part
+        else:
+            return jsonify({'status': 'invalid_token'}), 400  # No file part
     else:
-        return jsonify({'status': 'no_token_found'}), 400 # No file part
+        return jsonify({'status': 'no_token_found'}), 400  # No file part
+
 
 @app.route("/api/thumbs/<path:kartinka>", methods=["GET"])
 @cross_origin()
@@ -291,14 +306,104 @@ def get_thumb(kartinka):
         if sql_does_image_exist(f'{user}/{kartinka}'):
             image_path = os.path.join(f'thumb/users/{user}/{kartinka}')
             if not os.path.exists(image_path):
-                return jsonify({'status': 'error'}), 400 # No such file
+                return jsonify({'status': 'error'}), 400  # No such file
 
             return send_file(image_path, mimetype='image/jpeg')
-        else: 
-            return jsonify({'status': 'invalid_token'}), 400 # No file part
+        else:
+            return jsonify({'status': 'invalid_token'}), 400  # No file part
     else:
-        return jsonify({'status': 'no_token_found'}), 400 # No file part
+        return jsonify({'status': 'no_token_found'}), 400  # No file part
 
+# ------------------------------------------------------------------------------------
+# TELEGRAM_BOT:
+
+
+TELEGRAM_BOT_TOKEN = "6444851678:AAFz6FJlzxsLzudQGpMPbREQKWpMaWeKYa4"
+# ПРОБРОС:
+# https://api.telegram.org/bot6444851678:AAFz6FJlzxsLzudQGpMPbREQKWpMaWeKYa4/setWebhook?url=<URL_ИЗ_НГРОКА>
+
+
+def tel_parse_message(message):
+    print("message-->", message)
+    try:
+        chat_id = message['message']['chat']['id']
+        txt = message['message']['text']
+        print('\nText sender: ' +
+              str(message['message']['from']
+                  ['username'] + " " + str(chat_id)) + " " + str(chat_id))
+        # print("chat_id-->", chat_id)
+        print("txt-->", txt)
+        print('\n')
+        return chat_id, txt
+    except:
+        print("NO text found-->>")
+
+
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN + '/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    r = requests.post(url, json=payload)
+    return r
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        try:
+            chat_id, txt = tel_parse_message(msg)
+            tel_login = str(msg['message']['from']['username'])
+            telegram_code = str(msg['message']['text'])
+
+            if telegram_code == str(sql_get_user_telegram_authkey(tel_login)):
+                status_msg = "You are now an authorized user."
+                sql_set_user_auth_status(tel_login, 1)
+            else:
+                status_msg = "Wrong key."
+
+            mess_to_send = status_msg
+            tel_send_message(
+                chat_id, mess_to_send)
+        except:
+            print("from index(exception)-->")
+
+    return Response('ok', status=200)
+
+
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+# TELEGRAM_METHODS:
+
+
+@app.route("/api/get_auth_status/", methods=["POST"])
+def get_auth_status():
+    # {token}
+    data = request.json
+    data_error_check(data)
+
+    username = sql_token_to_user(data.get('token'))
+
+    return_value = str(sql_get_user_auth_status(username))
+    return jsonify({'status': return_value}), 200
+
+
+@app.route("/api/get_auth_key/", methods=["POST"])
+def get_auth_key():
+    # {token}
+    data = request.json
+    data_error_check(data)
+
+    username = sql_token_to_user(data.get('token'))
+
+    return_value = str(sql_get_user_telegram_authkey(username))
+    return jsonify({'status': return_value}), 200
+
+
+# ------------------------------------------------------------------------------------
 # AUTH
 appConf = {
     "OAUTH2_CLIENT_ID": "116203304654-fp8noi61ff6kdo5gnu40b7d41nbojn6b.apps.googleusercontent.com",
@@ -313,37 +418,41 @@ app.secret_key = appConf.get("FLASK_SECRET")
 oauth = OAuth(app)
 
 oauth.register(
-  name="hackmedia",
-  client_id=appConf.get("OAUTH2_CLIENT_ID"),
-  client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
-  client_kwargs={
-    "scope": "openid profile email",
+    name="hackmedia",
+    client_id=appConf.get("OAUTH2_CLIENT_ID"),
+    client_secret=appConf.get("OAUTH2_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
     },
     server_metadata_url=f'{appConf.get("OAUTH2_META_URL")}',
-    )
+)
 # -----
 
 # AUTH_APP_ROUTES:
+
+
 @app.route("/home")
 @cross_origin()
 def home():
-  return render_template("home.html", session=session.get("user"), info=json.dumps(session.get("user"), indent=4))
+    return render_template("home.html", session=session.get("user"), info=json.dumps(session.get("user"), indent=4))
+
 
 @app.route("/signin-google")
 @cross_origin()
 def googleCallback():
-  token = oauth.hackmedia.authorize_access_token()
-  session["user"] = token
-  return redirect(url_for("home"))
+    token = oauth.hackmedia.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for("home"))
 
 
 @app.route("/google-login")
 @cross_origin()
 def googleLogin():
-  if "user" in session:
-      abort(404)
-  return oauth.hackmedia.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
-  
+    if "user" in session:
+        abort(404)
+    return oauth.hackmedia.authorize_redirect(redirect_uri=url_for("googleCallback", _external=True))
+
+
 @app.route("/logauth")
 @cross_origin()
 def logauth():
@@ -354,35 +463,45 @@ def logauth():
 
 # Functions:
 
-def allowed_file(filename): 
-    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'webp', 'bmp', 'mp4', 'avi', 'mov', 'mkv'}
-    file_extension = filename.rsplit('.', 1)[1].lower() if '.' in filename else ''
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png',
+                          'webp', 'bmp', 'mp4', 'avi', 'mov', 'mkv'}
+    file_extension = filename.rsplit(
+        '.', 1)[1].lower() if '.' in filename else ''
     return '.' in filename and file_extension in ALLOWED_EXTENSIONS
 
-def is_video_file(filename): 
+
+def is_video_file(filename):
     ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
-    file_extension = filename.rsplit('.', 1)[1].lower() if '.' in filename else ''
+    file_extension = filename.rsplit(
+        '.', 1)[1].lower() if '.' in filename else ''
     return '.' in filename and file_extension in ALLOWED_EXTENSIONS
 
-def list_folders_in_directory(directory_path): 
+
+def list_folders_in_directory(directory_path):
     try:
         items = os.listdir(directory_path)
-        folders = [item for item in items if os.path.isdir(os.path.join(directory_path, item))]
+        folders = [item for item in items if os.path.isdir(
+            os.path.join(directory_path, item))]
         return folders
     except OSError as e:
         print(f"Error: {e}")
         return []
 
-def create_folder(directory): 
+
+def create_folder(directory):
     os.makedirs(directory, exist_ok=True)
 
-def delete_folder(directory): 
+
+def delete_folder(directory):
     shutil.rmtree(directory)
+
 
 def data_error_check(data):
     data = request.get_json()
     if not data:
-        return jsonify({'status': 'error'}), 400 # No JSON data provided
+        return jsonify({'status': 'error'}), 400  # No JSON data provided
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=appConf.get("FLASK_PORT"))
--- Project/sql_funct.py
+++ Project/sql_funct.py
@@ -3,29 +3,34 @@ from sql_connection import conn
 import random
 
 
-
 def sql(string):
     try:
         return conn.execute(text(string))
     except Exception as e:
         print(f"Can't do that\n error discription: {e}")
 
+
 def sql_token_exists_in_db(token):
     result = 0
     try:
-        result = sql(f"select EXISTS(select token from session where token = '{token}')").scalar()
+        result = sql(
+            f"select EXISTS(select token from session where token = '{token}')").scalar()
     except:
         result = 0
     return int(result)
 
 
 def sql_username_exists(username):
-    result = sql(f"select EXISTS(select name from users where name = '{username}')").scalar()
-    return int(result) # ok, 10/10 very good
+    result = sql(
+        f"select EXISTS(select name from users where name = '{username}')").scalar()
+    return int(result)  # ok, 10/10 very good
+
 
 def sql_get_user_password_hash(username):
-    result = sql(f"select password from users where name = '{username}'").scalar()
+    result = sql(
+        f"select password from users where name = '{username}'").scalar()
     return result
+
 
 def sql_add_user(username, password):
     id = sql(f"select count(id) from users").scalar()
@@ -33,15 +38,19 @@ def sql_add_user(username, password):
     sql(f"insert into session values(NULL, {id})")
     sql(f"insert into avatar values(NULL, {id})")
 
+
 def sql_set_user_auth_status(username, status):
-    sql(f"update users set accepted_status = {status} where name = '{username}'")
-    
+    sql(
+        f"update users set accepted_status = {status} where name = '{username}'")
+
 
 def sql_get_all_user_pictures_with_pattern(username, pattern):
     pattern = str(pattern)
     parsed = pattern.replace('*', '%')
-    result = sql(f"select p.path from pictures p join users u on (p.user_id = u.id) where u.name = '{username}' and p.path like '{parsed}'").fetchall()
+    result = sql(
+        f"select p.path from pictures p join users u on (p.user_id = u.id) where u.name = '{username}' and p.path like '{parsed}'").fetchall()
     return result
+
 
 def sql_post_image_location(username, file_location):
     id_user = sql(f"select id from users where name = '{username}'").scalar()
@@ -49,63 +58,90 @@ def sql_post_image_location(username, file_location):
 
 
 def sql_get_user_telegram_authkey(username):
-    result = sql(f"select telegram_code from users where name = '{username}'").scalar()
+    try:
+        result = sql(
+            f"select telegram_code from users where name = '{username}'").scalar()
+    except:
+        result = None
     return result
+
 
 def sql_change_auth_token(username, token):
     id_user = sql(f"select id from users where name = '{username}'").scalar()
     # sql(f"update session set token = '{token}' where user_id = {id_user}")
     sql(f"insert into session values ('{token}', {id_user})")
 
+
 def sql_token_to_user(token):
     try:
-        id = sql(f"select user_id from session where token = '{token}'").scalar()
+        id = sql(
+            f"select user_id from session where token = '{token}'").scalar()
         name = sql(f"select name from users where id = {id}").scalar()
         return name
     except Exception as e:
         print("Failed to cum", e)
 
+
 def sql_token_to_user_id(token):
     try:
-        id = sql(f"select user_id from session where token = '{token}'").scalar()
+        id = sql(
+            f"select user_id from session where token = '{token}'").scalar()
         return id
     except Exception as e:
         print("Failed to cum 2", e)
+
 
 def sql_change_image_location(user_id, filename_from, filename_to):
     sql(f"update pictures set path = '{filename_to}' where path = '{filename_from}' and user_id = '{user_id}'")
 
 ###
+
+
 def sql_does_image_exist(path):
-    result = sql(f"select EXISTS(select path from pictures where path = '{path}')")
+    result = sql(
+        f"select EXISTS(select path from pictures where path = '{path}')")
     return result
 
+
 def sql_remove_image_location(user_id, filename):
-    sql(f"delete from pictures where path = '{filename}' and user_id = '{user_id}'")
+    sql(
+        f"delete from pictures where path = '{filename}' and user_id = '{user_id}'")
+
 
 def sql_remove_image_by_pattern(user_id, pat):
-    sql(f"delete from pictures where path like '{pat}' and user_id = '{user_id}'")
+    sql(
+        f"delete from pictures where path like '{pat}' and user_id = '{user_id}'")
+
 
 def sql_change_avatar(username, filename):
     id_user = sql(f"select id from users where name = '{username}'").scalar()
     sql(f"update avatar set ava_path = '{filename}' where user_id = {id_user}")
 
+
 def sql_get_avatar(username):
     result = 0
     try:
-        id_user = sql(f"select id from users where name = '{username}'").scalar()
-        result = sql(f"select ava_path from avatar where user_id = '{id_user}'").scalar()
+        id_user = sql(
+            f"select id from users where name = '{username}'").scalar()
+        result = sql(
+            f"select ava_path from avatar where user_id = '{id_user}'").scalar()
     except:
         result = None
     return result
+
 
 def sql_delete_token(token):
     sql(f"delete from session where token = '{token}'")
 
 
 def sql_get_user_auth_status(username):
-    result = sql(f"select accepted_status from users where name = '{username}'")
+    try:
+        result = sql(
+            f"select accepted_status from users where name = '{username}'").scalar()
+    except:
+        result = None
     return result
+
 
 def print_table(table_name):
     '''
@@ -115,5 +151,5 @@ def print_table(table_name):
     session
     avatar
     '''
-    print( sql(f"select * from {table_name}").fetchall())
-    print("Len:",len(sql(f"select * from {table_name}").fetchall()))
+    print(sql(f"select * from {table_name}").fetchall())
+    print("Len:", len(sql(f"select * from {table_name}").fetchall()))
